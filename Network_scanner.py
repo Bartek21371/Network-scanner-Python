58ae2cf2e0fa429219c9fab05e22935886e4fca9
Bartosz <103534401+Bartek21371@users.noreply.github.com>
2024-07-27 21:07:02 +0200

    Network_scanner.py

--- Network_scanner.py
+++ Network_scanner.py
@@ -1,14 +1,20 @@
 import socket
-from ipaddress import ip_network 
+from ipaddress import ip_network
 from threading import Thread
 
-# Function for scan range of IP addresses and find active devices
-
+# Function for scanning range of IP addresses and finding active devices
 def scan_ip_range(network):
     active_ips = []
-    for ip in ip_network(network).hosts:
+    threads = []
+
+    for ip in ip_network(network).hosts():
         t = Thread(target=check_ip, args=(str(ip), active_ips))
+        threads.append(t)
         t.start()
+
+    for t in threads:
+        t.join()
+
     return active_ips
 
 def check_ip(ip, active_ips):
@@ -19,12 +25,18 @@ def check_ip(ip, active_ips):
         pass
 
 # Function to scan open ports on a specific IP address
-
 def scan_ports(ip, port_range):
     open_ports = []
+    threads = []
+
     for port in port_range:
         t = Thread(target=check_port, args=(ip, port, open_ports))
+        threads.append(t)
         t.start()
+
+    for t in threads:
+        t.join()
+
     return open_ports
 
 def check_port(ip, port, open_ports):
@@ -34,16 +46,16 @@ def check_port(ip, port, open_ports):
     if result == 0:
         open_ports.append(port)
     sock.close()
-    
-# Main function which combine IP addresses range and port scanning 
 
+# Main function which combines IP addresses range and port scanning
 def main(network, port_range):
-    active_ips = []
+    active_ips = scan_ip_range(network)
     for ip in active_ips:
         open_ports = scan_ports(ip, port_range)
-        print(f"Active IP: {ip}\n Open Ports: {open_ports}")
+        print(f"Active IP: {ip}\nOpen Ports: {open_ports}")
 
 if __name__ == "__main__":
     network = input("Input network to scan (In CIDR format example>192.168.1.0/24): ")
     port_range = range(20, 1025)
     main(network, port_range)
+    
